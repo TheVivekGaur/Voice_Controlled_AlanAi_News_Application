7a71466635b892feca66fdda3a4db6fc741f88ae
TheVivekGaur <vvksngh24@gmail.com>
2020-08-19 02:44:17 +0530

    adding more voice capabilites

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, {useEffect, useState}from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from '../src/components/NewsCards/NewsCards';
 import useStyles from './styles';
-const alanKey = '8c0b1408cbf5e09a38a713a0ed5363462e956eca572e1d8b807a3e2338fdd0dc/stage ';
+import wordsToNumbers from 'words-to-numbers';
+
+const alanKey = process.env.REACT_APP_ALAN_API_KEY;
 
 const App = () => {
  const [newsArticles, setNewsArticles] = useState([]);
+ const [activeArticle, setActiveArticle] =useState(-1);
  const classes = useStyles();
  useEffect(()=>{
    alanBtn({
          key: alanKey,
-         onCommand: ({command, articles}) =>{
-             if(command === 'newHeadlines')
-             {
+         onCommand: ({command, articles , number}) =>{
+             if(command === 'newHeadlines'){
                setNewsArticles(articles);
+               setActiveArticle(-1);
+             } else if (command === 'highlight'){
+                setActiveArticle((prevActiveArticle) => prevActiveArticle + 1 );
+             } else if(command === 'open'){
+                const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy:true}) : number;
+                const article = articles[parsedNumber - 1];
+
+                if(parsedNumber > 20){
+                  alanBtn().playText('Please try that again.')
+                } else if (article) {
+                  window.open(article.url, '_blank');
+                  alanBtn().playText('opening');
+                }
+         
              }
          }
    })
@@ -24,7 +40,7 @@ const App = () => {
   <div className={classes.logoContainer}>
   <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan-logo" />
   </div>
-  <NewsCards articles={newsArticles}  />
+  <NewsCards articles={newsArticles} activeArticle={activeArticle}  />
   </div>
  );
 }
